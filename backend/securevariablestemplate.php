fad28703a22da5808e26013a0f806e54e5ab9e04
DESKTOP-DCDSA5T\Brian <aldenbe@gmail.com>
2018-06-09 17:00:02 -0700

    v1.01
    Temp fix to auth logic to allow for www. or no subdomain on production server. Also fix to unreachable session cleanup.

--- backend/securevariablestemplate.php
+++ backend/securevariablestemplate.php
@@ -4,6 +4,13 @@
 const CLIENT_ID     = '';
 const CLIENT_SECRET = '';
 
+const WWW_CLIENT_ID     = '';
+const WWW_CLIENT_SECRET = '';
+
+const DEV_CLIENT_ID     = '';
+const DEV_CLIENT_SECRET = '';
+
+
 //mysql connect variables
 const SERVERNAME = '';
 const USERNAME = '';
